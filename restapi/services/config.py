fa718ef9fdc31883d75cb95cc120845635a2b909
IndominusByte <nyomanpradipta120@gmail.com>
2020-05-10 22:50:16 +0800

    update travis ci

--- restapi/services/config.py
+++ restapi/services/config.py
@@ -4,7 +4,7 @@ class Config:
     DEBUG = False
     SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/zooka-watersports'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/zooka_watersports'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_TOKEN_EXPIRES = 15 * 60  # 15 minute
     JWT_REFRESH_TOKEN_EXPIRES = 30 * 86400  # 30 days
