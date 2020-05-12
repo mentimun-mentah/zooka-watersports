3bdc3e9e67042d815e4ec7aba27410931541dd5b
IndominusByte <nyomanpradipta120@gmail.com>
2020-05-12 16:04:23 +0800

    update travis

--- restapi/services/tests/system/basetest.py
+++ restapi/services/tests/system/basetest.py
@@ -8,11 +8,4 @@ from app import app
 class BaseTest(unittest.TestCase):
     def setUp(self):
         app.config['JWT_SECRET_KEY'] = 'secretkey'
-        app.config['ACCESS_TOKEN_EXPIRES'] = 900
-        app.config['REFRESH_TOKEN_EXPIRES'] = 2592000
-        app.config['SMTP_SERVER'] = 'smtp.gmail.com'
-        app.config['SMTP_PORT'] = 465
-        app.config['SMTP_USE_SSL'] = True
-        app.config['SMTP_EMAIL'] = 'zookatest@gmail.com'
-        app.config['SMTP_PASSWORD'] = 'zooka2020'
         self.app = app.test_client
